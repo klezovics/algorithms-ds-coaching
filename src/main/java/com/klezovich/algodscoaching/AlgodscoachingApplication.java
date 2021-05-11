417811e0fcc6dea33157f7a05a1cf6260c5ce292
aklezovics <arturs.klezovics@raisin.com>
2021-05-11 20:27:02 +0200

    Intial commit

---
+++ src/main/java/com/klezovich/algodscoaching/AlgodscoachingApplication.java
@@ -0,0 +1,13 @@
+package com.klezovich.algodscoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgodscoachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgodscoachingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klezovich/algodscoaching/AlgodscoachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klezovich.algodscoaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgodscoachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
