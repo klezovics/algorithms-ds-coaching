191443626c22afab90f492c0eae8b28be8ed15e3
aklezovics <arturs.klezovics@raisin.com>
2021-05-11 20:37:58 +0200

    Add simple array example

---
+++ src/main/java/com/klezovich/algodscoaching/ds/OneDArray.java
@@ -0,0 +1,15 @@
+package com.klezovich.algodscoaching.ds;
+
+import java.util.Arrays;
+
+public class OneDArray {
+
+    public static void main(String[] args) {
+        var a = new int[5];
+
+        a[0]=1;
+        a[1]=2;
+
+        System.out.println(Arrays.toString(a));
+    }
+}
