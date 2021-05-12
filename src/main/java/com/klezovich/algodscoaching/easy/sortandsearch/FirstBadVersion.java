8f2b8733ff70678a77ea7e017fabd0d99aa31af4
aklezovics <arturs.klezovics@raisin.com>
2021-05-12 21:08:50 +0200

    Add placeholder

---
+++ src/main/java/com/klezovich/algodscoaching/easy/sortandsearch/FirstBadVersion.java
@@ -0,0 +1,12 @@
+package com.klezovich.algodscoaching.easy.sortandsearch;
+
+public class FirstBadVersion {
+
+    public boolean isBadVersion(int version) {
+        return true;
+    }
+
+    public int firstBadVersion(int n) {
+        return 0;
+    }
+}
