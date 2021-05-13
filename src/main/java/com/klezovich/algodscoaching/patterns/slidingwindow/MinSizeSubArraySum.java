4fbf1da6823670a9b3ccd98ce0dfc707eda9a6de
aklezovics <arturs.klezovics@raisin.com>
2021-05-13 22:28:18 +0200

    Add better solution

--- src/main/java/com/klezovich/algodscoaching/patterns/slidingwindow/MinSizeSubArraySum.java
+++ src/main/java/com/klezovich/algodscoaching/patterns/slidingwindow/MinSizeSubArraySum.java
@@ -8,59 +8,24 @@ import java.util.stream.Collectors;
 
 class MinSizeSubArraySum {
 
-  private static int[] array;
-  private static int targetSum;
 
   public static int findMinSubArray(int S, int[] arr) {
 
-    array=arr;
-    targetSum=S;
+      int windowStart=0;
+      int minLen = Integer.MAX_VALUE;
+      int windowSum=0;
 
-    Integer[] bestLenForStartAtPos= new Integer[arr.length];
+      for(int windowEnd=0; windowEnd<=arr.length-1; windowEnd++) {
+          windowSum+=arr[windowEnd];
 
-    //Find the shortest value for each position
-    for(int startPos=0; startPos<=arr.length-1; startPos++) {
-       bestLenForStartAtPos[startPos] = findMinLengthForSubarrayWithStartPos(startPos);
-    }
-
-    //Find the position with the best result;
-    List<Integer> vals = new ArrayList<>();
-
-    for(Integer val : bestLenForStartAtPos) {
-      if(val == null) {
-        continue;
+          while(windowSum >= S) {
+              minLen = Math.min(minLen, windowEnd - windowStart + 1);
+              windowSum -=arr[windowStart];
+              windowStart++;
+          }
       }
 
-      if(val == 0) {
-          continue;
-      }
-
-      vals.add(val);
-    }
-
-
-    //Sum is not reachable at all
-    if(vals.size() == 0) {
-      return 0;
-    }
-
-    return vals.stream().mapToInt(v->v).min().getAsInt();
+      return minLen == Integer.MAX_VALUE ? 0 : minLen;
   }
 
-  private static int findMinLengthForSubarrayWithStartPos(int startPos) {
-      int sum=0;
-      int pos=startPos;
-      int len=0;
-      while (sum < targetSum && pos < array.length) {
-        sum+=array[pos];
-        pos++;
-        len++;
-      }
-
-      if(sum < targetSum) {
-          return 0;
-      }
-
-      return len;
-  }
 }
